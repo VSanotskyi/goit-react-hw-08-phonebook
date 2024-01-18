5d38f1564c57b74a80c009d5202587e438b3b777
Vsanotskyi <vsanotskyi@gmail.com>
2024-01-18 04:39:44 +0200

    commit 10

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,7 +4,7 @@ import {
   selectError,
   selectIsLoading,
   selectIsLog,
-  selectIsRefresh, selectToken,
+  selectToken,
   selectUser,
 } from 'store/auth';
 
@@ -12,14 +12,12 @@ export const useAuth = () => {
   const authToken = useSelector(selectToken);
   const authUser = useSelector(selectUser);
   const authIsLog = useSelector(selectIsLog);
-  const authIsRefresh = useSelector(selectIsRefresh);
   const authIsLoading = useSelector(selectIsLoading);
   const authError = useSelector(selectError);
 
   return {
     authUser,
     authIsLog,
-    authIsRefresh,
     authIsLoading,
     authError,
     authToken,
